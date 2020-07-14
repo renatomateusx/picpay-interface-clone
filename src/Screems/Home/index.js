4bb74beec0a3108d5b3d6315f0f4dfa827703f04
Renato Mateus <renatomateusx@gmail.com>
2020-07-14 20:57:14 -0300

    Close Interface picpay

---
+++ src/Screems/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
+import Suggestions from '../../components/Suggestions';
+
+
+export default function Home() {
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+                    <BalanceContainer>
+                        <BalanceTitle>Meu Saldo</BalanceTitle>
+                        <Balance>R$ 0,00</Balance>
+                    </BalanceContainer>
+                    <AntDesign name="gift" size={30} color="#10c86e" />
+                </Header>
+                <Suggestions/>
+            </Container>
+        </Wrapper>
+    );
+}
