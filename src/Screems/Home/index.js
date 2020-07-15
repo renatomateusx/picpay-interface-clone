5a1c395db8b13280a54061745bf74a7c93dd7166
Renato Mateus <renatomateusx@gmail.com>
2020-07-15 10:01:22 -0300

    Finished interface home

--- src/Screems/Home/index.js
+++ src/Screems/Home/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
 import Suggestions from '../../components/Suggestions';
-
+import Activities from '../../components/Activities';
+import Tips from '../../components/Tips';
+import Banner from '../../components/Banner';
 
 export default function Home() {
     return (
@@ -18,6 +20,9 @@ export default function Home() {
                     <AntDesign name="gift" size={30} color="#10c86e" />
                 </Header>
                 <Suggestions/>
+                <Activities/>
+                <Tips/>
+                <Banner/>
             </Container>
         </Wrapper>
     );
